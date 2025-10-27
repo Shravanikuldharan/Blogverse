0cf4135d7335036148a23627f61321d93a7fc841
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-10-27 18:10:23 +0530

    Login and signup routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,8 @@ import AllBlogs from './views/AllBlogs'
 import NewBlog from './views/NewBlog'
 import EditBlog from './views/EditBlog'
 import ReadBlog from './views/ReadBlog'
+import Login from './views/Login'
+import Signup from './views/Signup'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
@@ -13,8 +15,8 @@ createRoot(document.getElementById('root')).render(
       <Route path="/new" element={<NewBlog />} />
       <Route path="/edit/:slug" element={<EditBlog />} />
       <Route path="/blog/:slug" element={<ReadBlog />} />
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route
         path="*"
         element={<h1 className="text-center mt-5">404 Not Found</h1>}
