d641c9010b35e7079104b4ffff4dbd01a8405ee3
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-10-27 17:39:40 +0530

    Libs and routes setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,25 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import AllBlogs from './views/AllBlogs'
+import NewBlog from './views/NewBlog'
+import EditBlog from './views/EditBlog'
+import ReadBlog from './views/ReadBlog'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AllBlogs />} />
+      <Route path="/new" element={<NewBlog />} />
+      <Route path="/edit/:slug" element={<EditBlog />} />
+      <Route path="/blog/:slug" element={<ReadBlog />} />
+      {/* <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} /> */}
+      <Route
+        path="*"
+        element={<h1 className="text-center mt-5">404 Not Found</h1>}
+      />
+    </Routes>
+  </BrowserRouter>
+
 )
