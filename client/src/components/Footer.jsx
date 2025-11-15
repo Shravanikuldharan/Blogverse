accc06b030036bc9d86d1f70ffa2def200568839
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-11-15 12:27:51 +0530

    Media query added to Navbar

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,23 +4,24 @@ import { Link } from "react-router";
 
 function Footer() {
   return (
-    <footer className="w-full bg-[#fff] text-[#0077b6] border-t border-[#cfe8ff] mt-10 py-6 bottom-0 left-0">
+    <footer className="w-full bg-[#fff] text-[#0077b6] border-t border-[#0077b6]/50 mt-10 py-6 bottom-0 left-0">
       <div className="max-w-6xl mx-auto flex flex-col sm:flex-row items-center justify-between px-6 sm:px-10 text-center sm:text-left gap-4">
 
-        <div className="flex items-center gap-3 flex-wrap justify-center sm:justify-start">
+        <div className="flex flex-col items-center sm:flex-row sm:items-center gap-3 sm:gap-2 justify-center sm:justify-start">
           <img
             src="/blog-logo.gif"
-            className="w-9 h-9 object-contain rounded-full shadow-sm"
+            className="w-8 h-8 sm:w-10 sm:h-10 object-contain rounded-full shadow-sm"
           />
 
-          <div className="flex items-center flex-wrap gap-2">
+          <div className="flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-2 text-center sm:text-left">
             <Link
               to="/"
-              className="text-xl font-bold tracking-wide text-[#0077b6] hover:text-[#005f8b] transition"
+              className="text-lg sm:text-xl font-bold tracking-wide text-[#0077b6] hover:text-[#005f8b] transition"
             >
               Blogverse
             </Link>
-            <span className="text-sm font-normal italic text-[#6b7280]">
+
+            <span className="text-xs sm:text-sm font-normal italic text-gray-500">
               - Your daily dose of blogs, stories & insights 🌿
             </span>
           </div>
